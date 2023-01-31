b26c91733ceb23c7627144d48b3226e85aeabbc8
usarthiharshini <usarthiharshini@gmail.com>
2023-01-31 16:18:26 +0530

    color generator

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,44 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color,setColor] = useState('#8F00FF');
+  const [list,setList] = useState([]);
+  const [error,setError] = useState(false)
+
+  const handleSubmit=(e)=>{
+
+    try {
+      e.preventDefault();
+      const values = new Values(color).all(10);
+      setList(values);
+      //console.log(list);
+    } catch (error) {
+      setError(true)
+      console.log(error);
+    }
+ 
+  }
+  return <>
+  <section className='container'>
+    
+        <h3>Color Generator</h3>
+       <input type='text' placeholder='#8F00FF'  onChange={(e)=>setColor(e.target.value)}
+       className={`${error && 'error'}`}/>
+        <button type="submit" className='btn' value={color}
+        onClick={handleSubmit}
+       >
+          generate</button>
+    
+    
+  </section>
+  <section className='colors'>
+   { 
+       list.map((color,index)=>{
+        return <SingleColor key={index} {...color} index={index} hex={color.hex}/>
+      })
+   
+   }
+  </section></>
 }
 
 export default App
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+//import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+
+
+
+const SingleColor = ({weight,rgb,index,hex}) => {
+
+
+ const [alert,setAlert] = useState(false)
+  const hexValue= '#'+hex;
+  
+  
+
+  useEffect(()=>{
+   let timeout = setTimeout(()=>{
+   setAlert(false)
+   },3000)
+   return ()=>clearTimeout(timeout)
+  } ,[alert])
+  return <article className={`color ${index>10 && 'color-light' }`} style={{backgroundColor:hexValue}}
+   onClick={(e)=>{setAlert(true)
+   navigator.clipboard.writeText(hexValue)}} key={index}>
+    <p className='percent-value' >{weight}%</p>
+    <p className='color-value'>{hexValue}</p>
+    <p className='alert'>{alert ? 'copied':''}</p>
+  </article>
 }
 
 export default SingleColor
